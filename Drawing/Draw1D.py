c270dd73fec2b005d1457f3678b36aef90175933
kokamido <kokamidokokosian@gmail.com>
2020-10-19 21:22:50 +0500

    Локальные поиски зависимости паттернов от длины отрезкаю Зачатки рисовалки

---
+++ Drawing/Draw1D.py
@@ -0,0 +1,29 @@
+from matplotlib import pyplot as plt
+from matplotlib import axes, rcParams
+from nptyping import NDArray
+from numpy import arange
+
+
+def set_defaults_1D():
+    rcParams['figure.figsize'] = (16,8)
+    rcParams['font.size'] = 22
+    rcParams['axes.grid'] = True
+    rcParams['grid.color'] = 'black'
+
+
+def set_xticks(ax: axes, left: float, right: float, step: float, var_name:str = None) -> axes:
+    xticks = arange(left, right+step, step)
+    ax.set_xticks(xticks)
+    if var_name:
+        xticks[-1] = var_name
+    ax.set_xtickslabels(xticks)
+    return ax
+
+
+def set_yticks(ax: axes, bot: float, top: float, step: float, var_name:str = None) -> axes:
+    yticks = arange(bot, top+step, step)
+    ax.set_yticks(yticks)
+    if var_name:
+        yticks[-1] = var_name
+    ax.set_ytickslabels(yticks)
+    return ax
